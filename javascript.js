56f73ed0ee1cc562fee400742a6f8b9e599232a4
NicholasTsou <nicholastsoukatos@gmail.com>
2023-04-20 22:12:42 -0400

    Started

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const allClear = document.getElementById('allclear');
+const equalBtn = document.getElementById('equal');
+const decimalBtn =  document.getElementsByClassName('decimal');
+const deleteBtn = document.getElementsByClassName('delete');
+const operators = document.querySelectorAll('operators');
+const numbers = document.querySelectorAll('numbers');
+const firstOperand = document.getElementsByClassName('first-operand');
+const secondOperand = document.getElementsByClassName('second-operand');
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = null;
+let finalNumber = null;
+
+function showNumber(button){
+    
+    finalNumber = null;
+
+}
+
+
