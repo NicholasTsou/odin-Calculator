ac09134fc000b22ab3e4272799bdbace88cc9f1d
NicholasTsou <nicholastsoukatos@gmail.com>
2023-04-26 16:38:49 -0400

    Continuing

--- javascript.js
+++ javascript.js
@@ -1,21 +1,92 @@
 const allClear = document.getElementById('allclear');
 const equalBtn = document.getElementById('equal');
-const decimalBtn =  document.getElementsByClassName('decimal');
-const deleteBtn = document.getElementsByClassName('delete');
-const operators = document.querySelectorAll('operators');
-const numbers = document.querySelectorAll('numbers');
-const firstOperand = document.getElementsByClassName('first-operand');
-const secondOperand = document.getElementsByClassName('second-operand');
+const decimalBtn =  document.getElementById('decimal');
+const deleteBtn = document.getElementById('delete');
+const operators = document.querySelectorAll('.operators');
+const numbers = document.querySelectorAll('.number');
+const firstOperand = document.querySelector('#first-operand');
+const secondOperand = document.querySelector('#second-operand');
 
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = null;
 let finalNumber = null;
 
+console.log(allClear);
+console.log(equalBtn);
+console.log(decimalBtn);
+console.log(deleteBtn);
+console.log(operators);
+console.log(numbers);
+console.log(firstOperand);
+console.log(secondOperand);
+
+
 function showNumber(button){
     
     finalNumber = null;
 
+    firstNumber = Number(firstNumber + button.innerText);
+
+    secondOperand.innerText = firstNumber;
+}
+
+function showDecimal(){
+
+    if(!firstNumber.toString().includes('.')){
+        firstNumber += '.';
+    }
+
+    return secondOperand.innerText = firstNumber;
+}
+
+function deleteAll(){
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = null;
+    finalNumber = null;
+    secondOperand.innerText = null;
+    firstOperand.innerText = null;
+}
+
+function deleteOne(){
+
+    firstNumber =  firstNumber.toString().slice(0, -1);
+    
+    return secondOperand.innerText = firstNumber;
+}
+
+function showOperator(button){
+    
 }
 
 
+function operate(a, b, operand){
+
+    if(operand === "+"){
+        return a + b;
+    }else if(operand === "-"){
+        return a - b;
+    }else if(operand === "*"){
+        return a * b;
+    }else if(operand === "÷"){
+        return a/b;
+    }
+}
+
+
+numbers.forEach(button =>{
+    button.addEventListener('click', () => showNumber(button));
+});
+
+decimalBtn.addEventListener("click", () => {
+    showDecimal();
+});
+
+allClear.addEventListener("click", () =>{
+    deleteAll();
+});
+
+deleteBtn.addEventListener("click", () =>{
+    deleteOne();
+});
